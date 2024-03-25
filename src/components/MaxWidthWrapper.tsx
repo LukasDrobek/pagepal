0e35cb1b0da2ab4fab2a92ef7a26682e8ee48b3d
Lukas Drobek <lukas.drobek1@gmail.com>
2024-03-25 18:09:11 +0100

    initial commit - project setup

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,24 @@
+// Responsive spacing wrapper component that sets a max-width for the content
+
+import { cn } from "@/lib/utils";
+
+const MaxWidthWrapper = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <div
+      className={cn(
+        "mx-auto w-full max-w-screen-xl px-2.5 md:px-20",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default MaxWidthWrapper;
