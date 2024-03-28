ae380370c8081b23a208f1c08e6a4712cf9f186a
Lukas Drobek <lukas.drobek1@gmail.com>
2024-03-28 17:40:31 +0100

    downgrade trcp

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,5 @@
+import { db } from "@/db";
+
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
 
@@ -7,6 +9,17 @@ const Page = async () => {
 
   //  Check if user is logging in for the first time
   if (!user || !user.id) redirect("/auth/callback?origin=/dashboard");
+
+  const dbUser = await db.user.findFirst({
+    where: {
+      id: user.id,
+    },
+  });
+
+  if (!dbUser) {
+    console.log("No DB user found, redirecting...");
+    redirect("/auth-callback?origin=dashboard");
+  }
 
   return (
     <div>
