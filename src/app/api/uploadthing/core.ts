638ad20407db45da9cb86d20e280eb36f7b04951
Lukas Drobek <lukas.drobek1@gmail.com>
2024-03-29 20:22:57 +0100

    vectorize pdf files upon upload

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,7 +1,11 @@
 import { db } from "@/db";
+import { getPineconeClient } from "@/lib/pinecone";
 
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
 
 const f = createUploadthing();
 
@@ -26,6 +30,46 @@ export const ourFileRouter = {
           uploadStatus: "PROCESSING",
         },
       });
+
+      // Index the PDF file in Pinecone (langchain)
+      try {
+        const res = await fetch(`https://utfs.io/f/${file.key}`);
+        const blob = await res.blob();
+        const loader = new PDFLoader(blob);
+
+        const pageContent = await loader.load();
+        const pagesNumber = pageContent.length;
+
+        // Vectorize the PDF
+        const pinecone = await getPineconeClient();
+        const pineconeIndex = pinecone.Index("pagepal");
+        const embeddings = new OpenAIEmbeddings({
+          openAIApiKey: process.env.OPENAI_API_KEY,
+        });
+
+        await PineconeStore.fromDocuments(pageContent, embeddings, {
+          pineconeIndex,
+          namespace: createdFile.id,
+        });
+
+        await db.file.update({
+          where: {
+            id: createdFile.id,
+          },
+          data: {
+            uploadStatus: "SUCCESS",
+          },
+        });
+      } catch (err) {
+        await db.file.update({
+          where: {
+            id: createdFile.id,
+          },
+          data: {
+            uploadStatus: "FAILED",
+          },
+        });
+      }
     }),
 } satisfies FileRouter;
 
