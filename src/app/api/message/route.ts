df130764fe4f7dee95562961d6651f4a1cf30568
Lukas Drobek <lukas.drobek1@gmail.com>
2024-03-29 20:41:17 +0100

    vectorize messages and implement real-time streaming

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,8 +1,13 @@
 import { db } from "@/db";
+import { openai } from "@/lib/openai";
 
 import { NextRequest, NextResponse } from "next/server";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { sendMessageValidator } from "@/lib/sendMessageValidator";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { getPineconeClient } from "@/lib/pinecone";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
+import { OpenAIStream, StreamingTextResponse } from "ai";
 
 // Endpoint for asking questions to the AI
 export const POST = async (req: NextRequest) => {
@@ -36,4 +41,84 @@ export const POST = async (req: NextRequest) => {
       fileId,
     },
   });
+
+  // Vectorize the message
+  const embeddings = new OpenAIEmbeddings({
+    openAIApiKey: process.env.OPENAI_API_KEY,
+  });
+
+  const pinecone = await getPineconeClient();
+  const pineconeIndex = pinecone.Index("pagepal");
+
+  const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+    pineconeIndex,
+    namespace: file.id,
+  });
+
+  // Search for pages similar to the message
+  const results = await vectorStore.similaritySearch(message, 3);
+
+  const prevMessages = await db.message.findMany({
+    where: {
+      fileId,
+    },
+    orderBy: {
+      createdAt: "asc",
+    },
+    take: 5,
+  });
+
+  const formattedMessages = prevMessages.map((m) => ({
+    role: m.isUserMessage ? ("user" as const) : ("assistant" as const),
+    content: m.text,
+  }));
+
+  // Send the results to OpenAI
+  const res = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    temperature: 0,
+    stream: true,
+    // OpenAI prompt
+    messages: [
+      {
+        role: "system",
+        content:
+          "Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format.",
+      },
+      {
+        role: "user",
+        content: `Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format. \nIf you don't know the answer, just say that you don't know, don't try to make up an answer.
+        
+      \n----------------\n
+  
+      PREVIOUS CONVERSATION:
+      ${formattedMessages.map((message) => {
+        if (message.role === "user") return `User: ${message.content}\n`;
+        return `Assistant: ${message.content}\n`;
+      })}
+      
+      \n----------------\n
+      
+      CONTEXT:
+      ${results.map((r) => r.pageContent).join("\n\n")}
+      
+      USER INPUT: ${message}`,
+      },
+    ],
+  });
+
+  const stream = OpenAIStream(res, {
+    async onCompletion(completion) {
+      await db.message.create({
+        data: {
+          text: completion,
+          isUserMessage: false,
+          userId,
+          fileId,
+        },
+      });
+    },
+  });
+
+  return new StreamingTextResponse(stream);
 };
