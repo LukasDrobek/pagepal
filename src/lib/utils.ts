558312094e785e31592b699b522715c3dabc58b3
Lukas Drobek <lukas.drobek1@gmail.com>
2024-04-02 19:31:32 +0200

    metadata

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -16,3 +16,48 @@ export function absoluteUrl(path: string) {
   // Otherwise, code is running on the server
   return `http://localhost:${process.env.PORT ?? 3000}${path}`;
 }
+
+// Contruct metadata for the page
+export function constructMetadata({
+  title = "PagePal",
+  description = "PagePal is an open-source software that makes chatting with youd documents easy.",
+  image = "/thumbnail.png",
+  icons = "/favicon.ico",
+  noIndex = false,
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+  noIndex?: boolean;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      // creator: "",
+    },
+    icons,
+    metadataBase: new URL("https://pagepal-ten.vercel.app"),
+    themeColor: "#ffffff",
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  };
+}
